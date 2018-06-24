fac058116fb73f5cc6daafcce8cf9bb8afed0288
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-24 16:13:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,7 +8,7 @@ class Owner
       :cats => []
     }
   end
-  
+
   def pets
     @pets
   end
@@ -24,23 +24,23 @@ class Owner
   def species
     @species
   end
-  
+
   def say_species
     "I am a human."
   end
-  
+
   def buy_fish(fish_name)
     @pets[:fishes] << fish_name
   end
-  
+
   def buy_cat(cat_name)
     @pets[:cats] << cat_name
   end
-  
+
   def buy_dog(dog_name)
     @pets[:dogs] << dog_name
   end
-  
+
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
