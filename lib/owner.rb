da3e6134934c936e9ce2b49a94c9ba7f76fd6653
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-24 16:13:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,47 @@
 class Owner
-  # code goes here
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
+  end
+  
+  def pets
+    @pets
+  end
 
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def species
+    @species
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def buy_fish(fish_name)
+    @pets[:fishes] << fish_name
+  end
+  
+  def buy_cat(cat_name)
+    @pets[:cats] << cat_name
+  end
+  
+  def buy_dog(dog_name)
+    @pets[:dogs] << dog_name
+  end
+  
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
  end
